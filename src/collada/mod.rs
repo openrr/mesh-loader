d69b141dd0908deff58a9531d2a58cbf91736e38
Taiki Endo <te316e89@gmail.com>
2024-01-30 21:33:29 +0900

    Move error helpers to their own modules

--- src/collada/mod.rs
+++ src/collada/mod.rs
@@ -5,6 +5,7 @@
 #![allow(clippy::wildcard_imports)] // TODO
 
 mod effect;
+mod error;
 mod geometry;
 mod image;
 mod instance;
@@ -883,62 +884,5 @@ impl FromStr for InputSemantic {
             "WEIGHT" => Self::WEIGHT,
             _ => bail!("unknown input semantic {:?}", s),
         })
-    }
-}
-
-mod error {
-    use super::*;
-
-    #[cold]
-    pub(super) fn one_or_more_elems(node: xml::Node<'_, '_>, name: &str) -> io::Error {
-        format_err!(
-            "<{}> element must be contain one or more <{}> elements ({})",
-            node.tag_name().name(),
-            name,
-            node.node_location()
-        )
-    }
-
-    #[cold]
-    pub(super) fn exactly_one_elem(node: xml::Node<'_, '_>, name: &str) -> io::Error {
-        format_err!(
-            "<{}> element must be contain exactly one <{}> element ({})",
-            node.tag_name().name(),
-            name,
-            node.node_location()
-        )
-    }
-
-    #[cold]
-    pub(super) fn multiple_elems(node: xml::Node<'_, '_>) -> io::Error {
-        format_err!(
-            "multiple <{}> elements ({})",
-            node.tag_name().name(),
-            node.node_location()
-        )
-    }
-
-    #[cold]
-    pub(super) fn unexpected_child_elem(child: xml::Node<'_, '_>) -> io::Error {
-        format_err!(
-            "unexpected child element <{}> in <{}> element ({})",
-            child.tag_name().name(),
-            child.parent_element().unwrap().tag_name().name(),
-            child.node_location()
-        )
-    }
-}
-
-mod warn {
-    use super::*;
-
-    #[cold]
-    pub(super) fn unsupported_child_elem(_child: xml::Node<'_, '_>) {
-        // warn!(
-        //     "<{}> child element in <{}> element is unsupported ({})",
-        //     child.tag_name().name(),
-        //     child.parent_element().unwrap().tag_name().name(),
-        //     child.node_location()
-        // );
     }
 }
--- src/stl/mod.rs
+++ src/stl/mod.rs
@@ -2,6 +2,8 @@
 //!
 //! [STL]: https://en.wikipedia.org/wiki/STL_(file_format)
 
+mod error;
+
 use std::{io, path::Path, str};
 
 use self::error::ErrorKind;
@@ -600,92 +602,5 @@ impl FromStl for Mesh {
 
     fn set_name(mesh: &mut Self::Context, name: &str) {
         mesh.name = name.to_owned();
-    }
-}
-
-mod error {
-    use std::{fmt, io, path::Path};
-
-    #[cfg_attr(test, derive(Debug))]
-    pub(super) enum ErrorKind {
-        // ASCII STL error
-        ExpectedSpace(&'static str, usize),
-        ExpectedNewline(&'static str, usize),
-        Expected(&'static str, usize),
-        Float(usize),
-        NotAscii(&'static str, usize),
-        // binary STL error
-        TooSmall,
-        InvalidSize,
-        TooManyTriangles,
-    }
-
-    impl ErrorKind {
-        #[cold]
-        #[inline(never)]
-        pub(super) fn into_io_error(self, start: &[u8], path: Option<&Path>) -> io::Error {
-            let remaining = match self {
-                // ASCII STL error
-                Self::Expected(.., n)
-                | Self::ExpectedNewline(.., n)
-                | Self::ExpectedSpace(.., n)
-                | Self::Float(n)
-                | Self::NotAscii(.., n) => n,
-                // binary STL error (always points file:1:1, as error occurs only during reading the header)
-                _ => start.len(),
-            };
-            crate::error::with_location(
-                &crate::error::invalid_data(self.to_string()),
-                &crate::error::Location::find(remaining, start, path),
-            )
-        }
-    }
-
-    impl fmt::Display for ErrorKind {
-        #[cold]
-        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            match *self {
-                // ASCII STL error
-                Self::ExpectedSpace(msg, ..) => {
-                    if msg == "normal" || msg == "vertex" {
-                        write!(f, "expected space before floats")
-                    } else {
-                        write!(f, "expected space after {msg}")
-                    }
-                }
-                Self::ExpectedNewline(msg, ..) => {
-                    if msg == "solid" {
-                        write!(f, "expected newline after solid name")
-                    } else if msg == "normal" || msg == "vertex" {
-                        write!(f, "expected newline after floats")
-                    } else {
-                        write!(f, "expected newline after {msg}")
-                    }
-                }
-                Self::Expected(msg, remaining) => {
-                    if msg == "solid" && remaining != 0 {
-                        write!(f, "expected solid or eof")
-                    } else if msg == "endsolid" {
-                        write!(f, "expected facet normal or endsolid")
-                    } else {
-                        write!(f, "expected {msg}")
-                    }
-                }
-                Self::Float(..) => write!(f, "error while parsing a float"),
-                Self::NotAscii(..) => write!(f, "invalid ASCII"),
-                // binary STL error
-                Self::TooSmall => write!(
-                    f,
-                    "failed to determine STL storage representation: \
-                     not valid ASCII STL and size is too small as binary STL"
-                ),
-                Self::InvalidSize => write!(
-                    f,
-                    "failed to determine STL storage representation: \
-                     not valid ASCII STL and size is invalid as binary STL"
-                ),
-                Self::TooManyTriangles => write!(f, "too many triangles"),
-            }
-        }
     }
 }
